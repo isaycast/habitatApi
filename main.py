063afb926160727c9de6503ea0f62084e0bba2ea
Isay Castañeda <isaycastaneda@Isays-MacBook-Pro.local>
2022-11-25 23:56:28 -0600

    initial commit model setup

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask 
+from flask import  request
+import joblib
+
+app = Flask(__name__)
+
+filename = './knn_polution_density_model.sav'
+
+proximity_model = joblib.load(filename)
+
+@app.route('/proximity', methods=['GET'])
+def hello():
+    args = request.args
+    print(args)
+    proximity_city = proximity_model.predict([[float(args.get('pollution')), float(args.get('airCo'))]])
+    
+    return str(proximity_city[0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
